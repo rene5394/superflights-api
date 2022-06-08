575ac0904a4ff4746e3e24179585fb62140246cc
Rene Torres <rene_edgardo_2@hotmail.com>
2022-06-07 21:43:48 -0600

    MongoDB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ['.env.development'],
     isGlobal: true
-  }), MongooseModule.forRoot(process.env.URI_MONGODB)
+  }), MongooseModule.forRoot(process.env.URI_MONGODB), UserModule
 ],
   controllers: [AppController],
   providers: [AppService],
