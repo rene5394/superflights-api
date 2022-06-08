8e76893efdc6e811a3087607458dd5e6721b2fde
Rene Torres <rene_edgardo_2@hotmail.com>
2022-06-07 21:21:31 -0600

    Global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter);
+  app.useGlobalInterceptors(new TimeoutInterceptor);
   await app.listen(3000);
 }
 bootstrap();
